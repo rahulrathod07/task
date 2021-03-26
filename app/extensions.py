23a0713e6f9261b1c490faf0502bab59745014ac
Rahul Rathod <rrahulrathod07@gmail.com>
2021-03-26 13:51:11 +0530

    changed app

--- app/extensions.py
+++ app/extensions.py
@@ -14,6 +14,7 @@ URI = "mysql://{}:{}@{}/{}".format(app.config['DB_USERNAME'],app.config['DB_PASS
 # app.config['SQLALCHEMY_DATBASE_URI'] = f"mysql://{app.config['DB_USERNAME']}:{app.config['DB_PASSWORD']}@{app.config['DB_SERVER_NAME']}/{app.config['DB_NAME']}"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b8d20ec0f53090:46cce531@us-cdbr-east-03.cleardb.com/heroku_a9c1dd5c5ec85b3'
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = URI
 
 

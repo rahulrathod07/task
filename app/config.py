124465695b0852902c6b781b531d7bc5b650885e
Rahul Rathod <rrahulrathod07@gmail.com>
2021-03-30 18:11:00 +0530

    structured file commit(30-03-2021)

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ URI = "mysql://{}:{}@{}/{}".format(
     os.environ.get('DB_SERVER_NAME'),
     os.environ.get('DB_NAME')
 )
-app.config['SQLALCHEMY_DATABASE_URI'] = URI
+# app.config['SQLALCHEMY_DATABASE_URI'] = URI
+app.config['SQLALCHEMY_DATABSE_URI'] = os.environ.get('CLEARDB_DATABASE_URL')
 db = SQLAlchemy(app)
